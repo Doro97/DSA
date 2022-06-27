6d3604cbe3724609f10fa3dbeb5bfe286e7290e2
dorothy <dorothycheruiyot97@gmail.com>
2022-06-27 22:08:36 +0300

    images

--- compareTriplets.py
+++ compareTriplets.py
@@ -13,51 +13,27 @@ def compareTriplets(a,b):
     # if a[i] < b[i]=Bob gets 1 point
     # if a[i] = b[i]=neither gets a point
     # indexes of each element =a.index(i)
-    # a=[]
-    # b[3]=[]
-    # for index,value in enumerate(a):
-    #     a_element=a[index]
-    
-    # for i, val in enumerate(b):
-    #     b_element= b[i]
-    
-    # while (a_element > b_element):
-    #     print (comparison_scores)
 
-    for (a_element,b_element) in zip (a,b):
-        comparison_scores=[]
-        comparison_scores.append(1)
-           
-        if (a_element > b_element):
-            score=[]            
-            for i in comparison_scores:                 
-                score.append(i)
+
+    valueList=[value for value in zip(a,b)]
+    # print (valueList[0][1])
+    j=0
+    while j <= 2:
+        score = []
+        for i in valueList:
+            if (valueList[j][0] > valueList[j][1]) :           
+                score.append(1)
                 print(score)
-                # print(comparison_scores)
-        elif (a_element < b_element):
-            b_score=[]
-            for j in comparison_scores:
-                b_score.append(j)
-                # print(score)
-        else:
-            pass
+            elif(valueList[j][0] < valueList[j][1]) :
+                score.append(1)
+                print(score)
+            else:
+                score.append(0)
+                print(score)
+            j += 1
+            
 
 
-
-        
-    # if (a_element > b_element):
-    #     print(a_element)
-    #     # print (score.append(comparison_scores))
-    # elif (a_element < b_element):
-    #     print(b_element)
-    #     # print (score.append(comparison_scores))
-    # else:
-    #     return 0
-            # a_ele=a[index]
-            # b_ele=b[i]
-            # print (a_ele,b_ele)
-            # print (a[index],b[i])
-
-# compareTriplets([5,6,7],[3,6,10])
+compareTriplets([5,6,7],[3,6,10])
 # compareTriplets([1,2,3],[3,2,1])
-compareTriplets([17, 28, 30],[99 ,16 ,8])
+# compareTriplets([17, 28, 30],[99 ,16 ,8])
